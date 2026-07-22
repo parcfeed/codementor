c97640443e06c3f5609f364cc1df49abc919c21c
parcfeed <parcfeed@gmail.com>
2026-07-22 15:50:23 +0000

    layout: ajoute suppressHydrationWarning sur <body> (extensions navigateur)

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
   return (
     <html lang="fr" suppressHydrationWarning>
       <body
+        suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
